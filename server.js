404f7504a298d9b341a4793744b7f98f9460acf7
Jessie <gisewaltzer@gmail.com>
2022-05-08 15:17:06 -0400

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const { Sequelize } = require("sequelize");
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+const databaseConnectionTest = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+databaseConnectionTest();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
 
 
